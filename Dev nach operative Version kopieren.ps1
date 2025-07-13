2d400144c9f94e6ead0749f8621256b0e980a7e7
Beat Arnet <beat.arnet@bluewin.ch>
2025-07-13 14:40:54 +0200

    Version 2.1

--- Dev nach operative Version kopieren.ps1
+++ Dev nach operative Version kopieren.ps1
@@ -12,7 +12,7 @@ param(
 )
 
 $RepoUrl = "https://github.com/BeatArnet/Arzttarif-Assistent"
-$Version = "v2.0"
+$Version = "v2.1"
 
 if (-not (Test-Path $DevPath)) {
     Write-Error "Entwicklungsverzeichnis '$DevPath' wurde nicht gefunden."
